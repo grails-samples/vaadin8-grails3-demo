9e5df45b29d3d658d2f82bb4ace0fa03c315abd3
Jeff Scott Brown <brownj@objectcomputing.com>
2017-10-25 09:53:43 -0500

    Use Interface instead of abstract Data Service class

--- grails-app/services/vaadin8/grails3/demo/CustomerService.groovy
+++ grails-app/services/vaadin8/grails3/demo/CustomerService.groovy
@@ -6,22 +6,16 @@ import grails.gorm.services.Where
 import java.time.LocalDate
 
 @Service(Customer)
-abstract class CustomerService {
+interface CustomerService {
 
-    List<Customer> findAllByNames(String name) {
-        if(!name) {
-            return list()
+    @Where({
+        if(name) {
+            firstName =~ "%${name}%" || lastName =~ "%${name}%"
         }
+    })
+    List<Customer> findAllByNames(String name)
 
-        searchByName name
-    }
+    Customer save(Customer contact)
 
-    @Where({ firstName =~ "%${name}%" || lastName =~ "%${name}%"})
-    abstract List<Customer> searchByName(String name)
-
-    abstract List<Customer> list()
-
-    abstract Customer save(Customer contact)
-
-    abstract Customer save(String firstName, String lastName, String phone, String email, Customer.CustomerStatus status, LocalDate birthDate)
+    Customer save(String firstName, String lastName, String phone, String email, Customer.CustomerStatus status, LocalDate birthDate)
 }
